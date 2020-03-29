8221534364454a01738a281e123705c48685f851
Nishanth Salinamakki <nrs3@illinois.edu>
2020-03-29 00:54:37 -0700

    Made few minor changes

--- tendies/build_exec_containers.sh
+++ tendies/build_exec_containers.sh
@@ -4,7 +4,6 @@
 echo "----------- BUILDING AND EXECUTING CONTAINERS ---------------"
 sudo `which docker-compose` down -v
 sudo `which docker-compose` up -d --build
-sudo docker ps -a
 sudo `which docker-compose` logs web > /tmp/web_docker_output.log
 sudo `which docker-compose` logs sql_db > /tmp/sql_db_docker_output.log
 sudo `which docker-compose` logs nosql_db > /tmp/nosql_db_docker_output.log
@@ -12,3 +11,5 @@ sudo `which docker-compose` logs nginx > /tmp/nginx_docker_output.log
 
 ./backup_postgres.sh
 ./backup_mongo.sh
+
+sudo docker ps -a
